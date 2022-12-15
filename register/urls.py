312caba19dc755e24c6be3b623cdfa62a0a67b6a
dasbairagya <dasbairagyagopal@gmail.com>
2022-12-15 16:07:29 +0530

    update login url

--- register/urls.py
+++ register/urls.py
@@ -3,8 +3,8 @@ from .views import RegisterCreateView, SigninRetrieveView, SingleUserView, EditU
 from django.urls import path, include
 
 urlpatterns = [
-    path('app/register/', RegisterCreateView.as_view(), name='register'),
-    path('app/signin/', SigninRetrieveView.as_view(), name='signin'),
-    path('app/editprofile/<int:pk>', EditUserView.as_view(), name='editprofile'),
-    path('app/viewprofile/<int:pk>', SingleUserView.as_view(), name='viewprofile'),
+    path('register/', RegisterCreateView.as_view(), name='register'),
+    path('signin/', SigninRetrieveView.as_view(), name='signin'),
+    path('editprofile/<int:pk>', EditUserView.as_view(), name='editprofile'),
+    path('viewprofile/<int:pk>', SingleUserView.as_view(), name='viewprofile'),
 ]
